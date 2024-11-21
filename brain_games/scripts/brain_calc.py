5755dc37c5e6e01b3c5e0b330f8b4acd9c2ae8aa
onSTRANN1Y <splashilia3@gmail.com>
2024-11-22 00:44:56 +0300

    Update brain_calc.py and brain_even.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from brain_games.scripts.brain_games import main
-from brain_games.cli import welcome_user
 import prompt
 import random
 
@@ -10,41 +9,34 @@ def calculator():
     print(f'Hello, {name}!')
     print('What is the result of the expression?')
     flag = 0
+    count = 0
     for i in range(3):
-        number1 = random.randint(1,50)
-        number2 = random.randint(1,25)
+        number1 = random.randint(1, 50)
+        number2 = random.randint(1, 25)
         operathion = random.choice('+-*')
-        print(f'Question:{number1} {operathion} {number2}')
-        answer = prompt.integer('You answer: ')
         if operathion == '+':
             flag = number1 + number2
-            if answer == flag:
-                print('Correct!')
-            else:
-                lose_game(answer,flag,name)
-                break
-        if operathion == '-':
+        elif operathion == '-':
             flag = number1 - number2
-            if answer == flag:
-                print('Correct!')
-            else:
-                lose_game(answer,flag,name)
-                break
-        if operathion == '*':
+        elif operathion == '*':
             flag = number1 * number2
-            if answer == flag:
-                print('Correct!')
-            else:
-                lose_game(answer,flag,name)
-                break
-    if i == 2:
+
+        print(f'Question:{number1} {operathion} {number2}')
+        answer = prompt.integer('You answer: ')
+
+        if flag == answer:
+            print('Correct!')
+            count += 1
+        else:
+            lose_game(answer, flag, name)
+            break
+    if count == 3:
         print(f'Congratulations, {name}!')
 
 
-def lose_game(answer,flag,name):
+def lose_game(answer, flag, name):
     print(f'{answer} is wrong answer ;(. Correct answer was {flag}.')
     print(f'''Let's try again, {name}!''')
-
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 from brain_games.scripts.brain_games import main
-from brain_games.cli import welcome_user
 import prompt
 import random
-
-
 
 
 def parity_check():
     name = main()
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    x = 0
+    count = 0
     flag = ''
     for i in range(3):
-        number = random.randint(1,100)
+        number = random.randint(1, 100)
         if number % 2 == 0:
             flag = 'yes'
         else:
@@ -23,19 +20,14 @@ def parity_check():
         answer = prompt.string('Your answer: ')
         if answer == flag:
             print('Correct!')
+            count += 1
         else:
             print(f'{answer} is wrong answer ;(. Correct answer was {flag}.')
             print(f'''Let's try again, {name}!''')
             break
-    if i == 2:
+    if count == 3:
         print(f'Congratulations, {name}!')
 
 
-def brain_games():
-    parity_check()
-
-
 if __name__ == '__main__':
-    brain_games()
-
-
+    parity_check()
