d8ec8aa61dc805c6f9cbaac838d074fed1ebb111
onSTRANN1Y <splashilia3@gmail.com>
2024-11-22 00:44:18 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,8 +2,8 @@ import prompt
 
 
 def welcome_user():
-        name = prompt.string('May I have your name? ')
-        return name
+    name = prompt.string('May I have your name? ')
+    return name
 
 
 if __name__ == '__main__':
