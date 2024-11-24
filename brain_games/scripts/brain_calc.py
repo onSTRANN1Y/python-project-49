d15902acec323ad2bf5e89cf1cb5885d7d022119
onSTRANN1Y <splashilia3@gmail.com>
2024-11-25 01:56:11 +0300

    Add scripts

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from brain_games.scripts.games.brain_calc import calculator
+
+def main ():
+    calculator()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from brain_games.scripts.games.brain_even import brain_even()
+
+def main ():
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
