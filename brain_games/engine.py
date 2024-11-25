4d79ffa2f6a00c034c2e1b3a73e8e8cf82447901
onSTRANN1Y <splashilia3@gmail.com>
2024-11-25 16:27:17 +0300

    made an engine for the game

---
+++ brain_games/engine.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+
+def game_engine(game_function, task_function):
+    name = welcome_user()
+    task_function()
+    for i in range(3):
+        correct_answer, answer = game_function()
+        if correct_answer == answer:
+            print('Correct!')
+        else:
+            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.')
+            print(f'''Let's try again, {name}!''')
+            return
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    game_engine()
