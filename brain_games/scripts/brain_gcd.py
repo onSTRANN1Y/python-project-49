aa80931e3a3ef2eeaf8df7ccd0dedf44fc716fcd
onSTRANN1Y <splashilia3@gmail.com>
2024-12-03 01:00:14 +0300

    formatted imports with isort

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+from brain_games.game_launcher import launching_the_engine
 from brain_games.games.gcd import get_correct_answer_and_task
 from brain_games.games.instructions import QUESTION_GCD_GAME
-from brain_games.game_launcher import launching_the_engine
 
 
 def main():
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from brain_games.games.prime import get_correct_answer_and_task
-from brain_games.games.instructions import QUESTION_PRIME_GAME
 from brain_games.game_launcher import launching_the_engine
+from brain_games.games.instructions import QUESTION_PRIME_GAME
+from brain_games.games.prime import get_correct_answer_and_task
 
 
 def main():
