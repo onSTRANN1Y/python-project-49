4ad2ced50a37db34d3b0205ecb86b8476b46364e
onSTRANN1Y <splashilia3@gmail.com>
2024-12-03 00:21:19 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games.even import brain_even, task
-from brain_games.engine import game_engine
+from brain_games.games.even import get_correct_answer_and_task
+from brain_games.games.instructions import QUESTION_EVEN_GAME
+from brain_games.game_launcher import launching_the_engine
 
 
 def main():
-    game_engine(brain_even, task)
+    launching_the_engine(get_correct_answer_and_task, QUESTION_EVEN_GAME)
 
 
 if __name__ == '__main__':
