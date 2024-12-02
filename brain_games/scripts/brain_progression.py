5cb9a68771468b9f6f62c3af9b88800e85bd4013
onSTRANN1Y <splashilia3@gmail.com>
2024-12-03 00:22:03 +0300

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games.progression import brain_progression, task
-from brain_games.engine import game_engine
+from brain_games.games.progression import get_correct_answer_and_task
+from brain_games.games.instructions import QUESTION_PROGRESSION_GAME
+from brain_games.game_launcher import launching_the_engine
 
 
 def main():
-    game_engine(brain_progression, task)
+    launching_the_engine(get_correct_answer_and_task, QUESTION_PROGRESSION_GAME)
 
 
 if __name__ == '__main__':
